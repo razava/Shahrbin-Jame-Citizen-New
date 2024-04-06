fd5abe8a14270520aa9cd3e999d865cc74cfa2d1
Hossein Rahmani <hosseinrahmani3007@gmail.com>
2024-04-06 17:13:59 +0330

    a bug fixed

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -72,7 +72,7 @@ export const appConstants = {
 export const API_URL =
   process.env.NODE_ENV === "development"
     ? process.env.REACT_APP_API_URL_DEV
-    : process.env.REACT_APP_API_URL_PROD;
+    : process.env.REACT_APP_API_URL_DEV;
 
 export const errorMessages = {
   500: "خطایی رخ داد. لطفا از اتصال اینترنت خود اطمینان حاصل نمایید.",
